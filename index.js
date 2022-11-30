d10958c1830e1f1c558456dfd19f327a4c799cf8
YumilRuedaFlores-Ksquare <Yumil.Flores@itksquare.edu.mx>
2022-11-30 23:40:35 +0100

    More corrections

--- index.js
+++ index.js
@@ -19,3 +19,33 @@ function scrollFunction() {
 
 
 
+var end = new Date('12/12/2022 11:00 AM');
+
+    var _second = 1000;
+    var _minute = _second * 60;
+    var _hour = _minute * 60;
+    var _day = _hour * 24;
+    var timer;
+
+    function showRemaining() {
+        var now = new Date();
+        var distance = end - now;
+        if (distance < 0) {
+
+            clearInterval(timer);
+            document.querySelector('#countdown').innerHTML = 'EXPIRED!';
+
+            return;
+        }
+        var days = Math.floor(distance / _day);
+        var hours = Math.floor((distance % _day) / _hour);
+        var minutes = Math.floor((distance % _hour) / _minute);
+        var seconds = Math.floor((distance % _minute) / _second);
+
+        document.querySelector('#countdown').innerHTML = days + ' days, ';
+        document.querySelector('#countdown').innerHTML += hours + ' hours, ';
+        document.querySelector('#countdown').innerHTML += minutes + ' minutes and ';
+        document.querySelector('#countdown').innerHTML += seconds + ' seconds';
+    }
+
+    timer = setInterval(showRemaining, 1000);
